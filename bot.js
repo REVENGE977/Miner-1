c037ebdf099a8b5f955be5640c322631a598736d
Discord.js Arabic Developer <32039975+Bowlingtoolkit@users.noreply.github.com>
2018-11-15 19:43:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,176 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const ServerID = "492747482942406656"; //اي دي السيرفر
+const ChannelID = "492747482942406659";// اي دي الروم
 
-console.log('Welcome to Probot Miner !')
-console.log('DONT USE THIS CODE ON YOUR REAL ACCOUNT !') // WARNING !
+console.log(`
+ 
 
-client.on('ready', async() => {
-    const server = "492747482942406656"; // ايدي السررفر
-const channel = "492747482942406659";//ايدي الروم
-        setInterval(()=>{
-        client.guilds.get(server).channels.get(channel).send(`*ssssssssssssssssssssdadasdadasidjapoidnadipkandsaidjha0piwdawnidkkkkkdkdkdkdkdkdkmsaddaopidwdmaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaawsakllmdsa;kkkkkdmsdk;samnkcnaksdnaaaaaaaaaasjfakjandjakdnakl
-        **`)//spamming message.
-    },305);//Delay Time
-    })
 
-client.login(process.env.BOT_TOKEN)//Your Token
 
-  
+                                                                                                            
+                                                                                                            
+  ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+ /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+                                                                                                            
+                                                                                                            
+
+		
+ ▒█▀▀█ █▀▀█ █▀▀█ █▀▀▄ █▀▀█ ▀▀█▀▀ 
+ ▒█▄▄█ █▄▄▀ █░░█ █▀▀▄ █░░█ ░░█░░ 
+ ▒█░░░ ▀░▀▀ ▀▀▀▀ ▀▀▀░ ▀▀▀▀ ░░▀░░ 
+         
+
+ ▒█▀▀█ █▀▀█ █▀▀ █▀▀▄ ░▀░ ▀▀█▀▀ █▀▀ 
+ ▒█░░░ █▄▄▀ █▀▀ █░░█ ▀█▀ ░░█░░ ▀▀█ 
+ ▒█▄▄█ ▀░▀▀ ▀▀▀ ▀▀▀░ ▀▀▀ ░░▀░░ ▀▀▀ 
+ 
+
+▒█▀▄▀█ ░▀░ █▀▀▄ █▀▀ █▀▀█ 
+▒█▒█▒█ ▀█▀ █░░█ █▀▀ █▄▄▀ 
+▒█░░▒█ ▀▀▀ ▀░░▀ ▀▀▀ ▀░▀▀ 
+
+        ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+        /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+`)
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+// ShaDoW_MaN
+console.log(`ShaDoW_MaN`)
+console.log(`
+______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+/_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╭╮╱╭━━━╮╱╱╱╱╱╱╭╮╭╮╱╱╱╱╭━╮╭━╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╭╯╰╮┃╭━╮┃╱╱╱╱╱╱┃┣╯╰╮╱╱╱┃┃╰╯┃┃
+┃╰━╯┣━┳━━┫╰━┳━┻╮╭╯┃┃╱╰╋━┳━━┳━╯┣╮╭╋━━╮┃╭╮╭╮┣┳━╮╭━━┳━╮
+┃╭━━┫╭┫╭╮┃╭╮┃╭╮┃┃╱┃┃╱╭┫╭┫┃━┫╭╮┣┫┃┃━━┫┃┃┃┃┃┣┫╭╮┫┃━┫╭╯
+┃┃╱╱┃┃┃╰╯┃╰╯┃╰╯┃╰╮┃╰━╯┃┃┃┃━┫╰╯┃┃╰╋━━┃┃┃┃┃┃┃┃┃┃┃┃━┫┃
+╰╯╱╱╰╯╰━━┻━━┻━━┻━╯╰━━━┻╯╰━━┻━━┻┻━┻━━╯╰╯╰╯╰┻┻╯╰┻━━┻╯    
+╭━━━╮╭╮╱╱╱╱╱╭╮
+┃╭━╮┣╯╰╮╱╱╱╭╯╰╮
+┃╰━━╋╮╭╋━━┳┻╮╭╋┳━╮╭━━╮
+╰━━╮┃┃┃┃╭╮┃╭┫┃┣┫╭╮┫╭╮┃
+┃╰━╯┃┃╰┫╭╮┃┃┃╰┫┃┃┃┃╰╯┃╭┳┳╮
+╰━━━╯╰━┻╯╰┻╯╰━┻┻╯╰┻━╮┃╰┻┻╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+
+______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+/_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+`)
+
+
+client.on('ready', () => console.log(`
+╭━━━╮╱╱╱╱╱╱╱╱╱╭━╮╭━╮╱╱╱╱╱╱╱╱╱╭━━━╮╭╮╱╱╱╱╱╭╮╱╱╱╱╱╭╮╭╮
+╰╮╭╮┃╱╱╱╱╱╱╱╱╱┃┃╰╯┃┃╱╱╱╱╱╱╱╱╱┃╭━╮┣╯╰╮╱╱╱╭╯╰╮╱╱╱╱┃┃┃┃
+╱┃┃┃┣━━┳━╮╭━━╮┃╭╮╭╮┣┳━╮╭━━┳━╮┃╰━━╋╮╭╋━━┳┻╮╭╋━━┳━╯┃┃┃
+╱┃┃┃┃╭╮┃╭╮┫┃━┫┃┃┃┃┃┣┫╭╮┫┃━┫╭╯╰━━╮┃┃┃┃╭╮┃╭┫┃┃┃━┫╭╮┃╰╯
+╭╯╰╯┃╰╯┃┃┃┃┃━┫┃┃┃┃┃┃┃┃┃┃┃━┫┃╱┃╰━╯┃┃╰┫╭╮┃┃┃╰┫┃━┫╰╯┃╭╮╭╮
+╰━━━┻━━┻╯╰┻━━╯╰╯╰╯╰┻┻╯╰┻━━┻╯╱╰━━━╯╰━┻╯╰┻╯╰━┻━━┻━━╯╰╯╰╯
+`));// ShaDoW_MaN
+
+client.on('disconnect', () => console.log(`
+╭━╮╭━╮╱╱╱╱╱╱╱╱╱╭╮╱╭╮╱╱╱╱╭╮
+┃┃╰╯┃┃╱╱╱╱╱╱╱╱╱┃┃╱┃┃╱╱╱╱┃┃             
+┃╭╮╭╮┣┳━╮╭━━┳━╮┃╰━╯┣━━┳━╯┃
+┃┃┃┃┃┣┫╭╮┫┃━┫╭╯┃╭━╮┃╭╮┃╭╮┃
+┃┃┃┃┃┃┃┃┃┃┃━┫┃╱┃┃╱┃┃╭╮┃╰╯┃
+╰╯╰╯╰┻┻╯╰┻━━┻╯╱╰╯╱╰┻╯╰┻━━╯
+
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╭┳╮
+╰╮╭╮┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╯╰╮╱╱╱╱┃┃┃
+╱┃┃┃┣┳━━┳━━┳━━┳━╮╭━╮╭━━┳━┻╮╭╋━━┳━╯┃┃
+╱┃┃┃┣┫━━┫╭━┫╭╮┃╭╮┫╭╮┫┃━┫╭━┫┃┃┃━┫╭╮┣╯
+╭╯╰╯┃┣━━┃╰━┫╰╯┃┃┃┃┃┃┃┃━┫╰━┫╰┫┃━┫╰╯┣╮
+╰━━━┻┻━━┻━━┻━━┻╯╰┻╯╰┻━━┻━━┻━┻━━┻━━┻╯
+
+`));
+
+client.on('reconnecting', () => console.log(`
+╭━━━╮╱╱╱╱╭╮╱╱╱╱╭╮╱╭━━━╮╱╱╱╱╱╱╭╮╭╮
+┃╭━╮┃╱╱╱╱┃┃╱╱╱╭╯╰╮┃╭━╮┃╱╱╱╱╱╱┃┣╯╰╮
+┃╰━╯┣━┳━━┫╰━┳━┻╮╭╯┃┃╱╰╋━┳━━┳━╯┣╮╭╋━━╮
+┃╭━━┫╭┫╭╮┃╭╮┃╭╮┃┃╱┃┃╱╭┫╭┫┃━┫╭╮┣┫┃┃━━┫
+┃┃╱╱┃┃┃╰╯┃╰╯┃╰╯┃╰╮┃╰━╯┃┃┃┃━┫╰╯┃┃╰╋━━┃
+╰╯╱╱╰╯╰━━┻━━┻━━┻━╯╰━━━┻╯╰━━┻━━┻┻━┻━━╯
+╭━╮╭━╮╱╱╱╱╱╱╱╱╱╭━━╮               
+┃┃╰╯┃┃╱╱╱╱╱╱╱╱╱╰┫┣╯
+┃╭╮╭╮┣┳━╮╭━━┳━╮╱┃┃╭━━╮
+┃┃┃┃┃┣┫╭╮┫┃━┫╭╯╱┃┃┃━━┫
+┃┃┃┃┃┃┃┃┃┃┃━┫┃╱╭┫┣╋━━┃
+╰╯╰╯╰┻┻╯╰┻━━┻╯╱╰━━┻━━╯           
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮
+┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╯╰╮
+┃╰━╯┣━━┳━━┳━━┳━╮╭━╮╭━━┳━┻╮╭╋┳━╮╭━━╮
+┃╭╮╭┫┃━┫╭━┫╭╮┃╭╮┫╭╮┫┃━┫╭━┫┃┣┫╭╮┫╭╮┃
+┃┃┃╰┫┃━┫╰━┫╰╯┃┃┃┃┃┃┃┃━┫╰━┫╰┫┃┃┃┃╰╯┣┳┳╮
+╰╯╰━┻━━┻━━┻━━┻╯╰┻╯╰┻━━┻━━┻━┻┻╯╰┻━╮┣┻┻╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+
+
+`));// ShaDoW_MaN
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
+
+
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+// ShaDoW_MaN
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// ShaDoW_MaN
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// ShaDoW_MaN
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// ShaDoW_MaN
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// ShaDoW_MaN
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
+
+
+    });
+}
+
+
+client.login(process.env.BOT_TOKEN)
